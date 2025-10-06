b74bcf8f7b011824cdca373a2917a9a458011be0
Sheband <oochov@gmail.com>
2025-10-06 16:18:19 +0300

    Test commit

---
+++ build/CMakeFiles/mainex.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mainex.dir/main.cpp.o"
+  "CMakeFiles/mainex.dir/main.cpp.o.d"
+  "mainex"
+  "mainex.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mainex.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/mainex.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for mainex.
+# This may be replaced when dependencies are built.
